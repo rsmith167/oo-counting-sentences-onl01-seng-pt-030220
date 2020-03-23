cbd2d91cb46bea2ef8e5f48754df5de972ffcfb1
Rwanda S <rwanda.ace@gmail.com>
2020-03-23 19:38:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,26 +16,10 @@ self.end_with?("!")
   end
 
   def count_sentences
-    sentence_array = ""
-     if sen.sentence?
-       sentence_array = sen.split(".")
-       elsif sen.question?
-       sentence_array = sen.split("?")
-       elsif sen.exclamation?
-       sentence_array = sen.split("!")
-      end
-    sentence_array.each do |string|
-      if  sentence_array.include?(".")
-      sentence_array << sentence_array[0].split(".")
-    end
-    if string.include?("?")
-      sentence_array << sentence_array.split("?")
-    end
-    if string.include?("!")
-      sentence_array << sentence_array.split("!")
-    end
-    sentence_array.count
-  end
+    
+    self.each do |letter|
+      letter == letter.capitalize
+    
   end
   
 end
