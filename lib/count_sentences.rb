eba01308b02fc52bd6c227e1dbb71b03b23fc16b
Rwanda S <rwanda.ace@gmail.com>
2020-03-23 20:45:45 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+   self.end_with?(".")
+     
   end
 
   def question?
-
+  self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
   def count_sentences
+    sen = ""
+    sen = self.split(" ")
+index_counter = []
+sen.each_with_index do |value, index|
+if value.sentence? || value.question? || value.exclamation?
+index_counter << index
+end
+end
+return index_counter.count
+	
 
-  end
+end
 end
