4d30b905280897602a727215811ad64442449c2a
Rwanda S <rwanda.ace@gmail.com>
2020-03-23 20:26:14 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -25,25 +25,17 @@ self.end_with?("!")
     sen = self.split(".")
     sen.count
     end
+    counter = 0
     place = sen.length
-    for( i = 0; i < sen.length; i++)
-    {
+    loop do
+    
+    sen[counter] = sen[counter].split(".")
+    sen[counter] = sen[counter].split("?")
     holder = sen.length
-    sen[i] = sen[i].split(".")
-    sen[i] = sen[i].split("?")
     if holder > place
-      i = 0
+      counter = 0
       place = sen.length
     end
-    }
-    
-    
-    
+  end 
   end
-  
-  
-  
-  
-  
-  
 end
